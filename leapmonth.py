0c8e8366691783941c224e2c120a15d7f4e46f4d
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2019-05-24 18:45:57 +0900

    All test pass

--- leapmonth.py
+++ leapmonth.py
@@ -1,8 +1,34 @@
 import sys
 
+def is_int(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+
 def main():
-    year = int(sys.argv[1])
-    month = int(sys.argv[2])
+    usage = 'illigal inputs; usage: python leapmonth.py year month'
+
+    if len(sys.argv) != 3:
+        print(usage)
+        return
+    
+
+    year = sys.argv[1]
+    month = sys.argv[2]
+
+    if not (is_int(year) and is_int(month)):
+        print(usage)
+        return
+    else:
+        year = int(year)
+        month = int(month)
+
+    
+    if year < 0 or year > 3000 or month < 1 or month > 12:
+        print(usage)
+        return
     if month == 2:
         if year % 4 == 0 and year % 100 != 0 or year % 400 == 0 :
             days = 29
